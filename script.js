a059408f3681618fc7b5205fd6ecef7ee5b64452
stavkinasport220-dot <stavkinasport220@gmail.com>
2026-04-10 04:48:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ function scrollToContact() {
         behavior: "smooth"
     });
 }
+
+/* SCROLL ANIMATIONS */
+const faders = document.querySelectorAll(".fade");
+
+window.addEventListener("scroll", () => {
+    faders.forEach(el => {
+        const rect = el.getBoundingClientRect();
+        if (rect.top < window.innerHeight - 100) {
+            el.classList.add("show");
+        }
+    });
+});
